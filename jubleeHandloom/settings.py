bcccee0a7f2ac00c7e8c0610d292d29845244687
anmolsingla <anmolsingla000001@gmail.com>
2021-10-22 17:35:56 +0530

    second commit

--- jubleeHandloom/settings.py
+++ jubleeHandloom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x8a^@yv*lwxx^^jk3(t7qa5+)h*e=mu@2=0@-as^rnh3_8n11%'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jubleehandloom.herokuapp.com", "localhost"]
 
 
 # Application definition
